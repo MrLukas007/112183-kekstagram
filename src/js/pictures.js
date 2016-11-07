517cef6e8b989b3739b6ba943d4281d40fb4ddb3
MrLukas007 <lukas007@bk.ru>
2016-11-07 13:18:21 +0300

    Сделано всё без рекурсивной функции подгрузки карточек с сервера. Сейчас загружается PAGE_SIZE = 6 карточек, чтобы загружались все - нужно поставить к примеру 26

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -8,11 +8,10 @@ var gallery = require('./gallery');
   var pageNumber = 0;
   var renderProperties = {
     from: pageNumber * PAGE_SIZE,
-    get to() { return this.from + PAGE_SIZE;},
+    to: pageNumber * PAGE_SIZE + PAGE_SIZE,
     filter: 'filter-popular'
   };
   var URL = 'http://localhost:1507/api/pictures';
-  var THROTTLE_DELAY = 100;
   var filter = document.querySelector('.filters');
   var container = document.querySelector('.pictures');
 
@@ -26,18 +25,11 @@ var gallery = require('./gallery');
     gallery.setPictures(images);
     filter.classList.remove('hidden');
   };
-//Удаление всех отрисованных элементов
-//   var deleteAllElements = function() {
-//     var arr = container.querySelectorAll('a');
-//     arr.forEach(function(elem) {
-//       container.removeChild(elem);
-//     });
-//   };
+
 //Применение фильтров
   var setFiltersEnabled = function() {
     filter.addEventListener('change', function(evt) {
       if (evt.target.name == 'filter') {
-        // deleteAllElements();
         container.innerHTML = '';
         pageNumber = 0;
         renderProperties.filter = evt.target.id;
@@ -49,48 +41,6 @@ var gallery = require('./gallery');
   load(URL, renderImages, renderProperties);
   setFiltersEnabled();
 
-
-  //
-  // var isNextPageAvailable = function(images, page, pageSize) {
-  //   return page < Math.floor(images.length / pageSize);
-  // };
-
-  // var isBottomReached = function() {
-  //   var GAP = 100;
-  //   var footerElement = document.querySelector('footer');
-  //   var footerPosition = footerElement.getBoundingClientRect();
-  //   return footerPosition.top - window.innerHeight - GAP <= 0;
-  // };
-  //
-  // var renderNextPages = function(reset) {
-  //   if (reset) {
-  //     pageNumber = 0;
-  //     container.innerHTML = '';
-  //   }
-  //
-  //   while(isBottomReached()) {
-  //     pageNumber++;
-  //     load(URL, renderImages, renderProperties);
-  //   }
-  //   // &&
-  //   // isNextPageAvailable(images, pageNumber, PAGE_SIZE)) {
-  //   //   load(URL, renderImages, renderProperties);
-  //   //   pageNumber++;
-  //   // }
-  // };
-  //
-  //
-  // var setScrollEnabled = function() {
-  //   var lastCall = Date.now();
-  //
-  //   window.addEventListener('scroll', function(evt) {
-  //     if (Date.now() - lastCall >= THROTTLE_DELAY) {
-  //       renderNextPages();
-  //       lastCall = Date.now();
-  //     }
-  //   });
-  // };
-  // setScrollEnabled();
 })();
 
 
